b67fe95761a635a0383a9273c40b2a791424f94e
Aaron Wolf <aaron.wolf@gmail.com>
2026-03-22 09:55:47 +0200

    llama

---
+++ week4/day1/jsonEx.js
@@ -0,0 +1,10 @@
+let myObj = {
+  name: 'Sydney',
+  location: 'Jerusalem',
+  profession: {
+    position: 'student',
+    employer: 'DI'
+  }
+};
+
+let myJson = '{"name": "sydney", "location" : "jerusalem"...}';
